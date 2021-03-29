70807baa50db79f2a41a25d009bcd9552068aee0
VitorBusnardo <75642129+VitorBusnardo@users.noreply.github.com>
2021-03-29 13:43:35 -0300

    Configuração Geral

--- app/js/contador.js
+++ app/js/contador.js
@@ -1,29 +1,32 @@
-function ativaContador(e){
+function ativaContador(e) {
+
     $('.contador').each(function() {
-                $(this).prop('Counter', 0).animate({
-                    Counter: $(this).text()
-                }, {
-                    duration: 8000,
-                    easing: 'swing',
-                    step: function(now) {
-                 
-                        $(this).text(Math.ceil(now));
-                    },
-                  complete: function(){
-                $(this).stop(true,true)
-                  },
-                });
-                
-            });
-      }
-    
-    $(window).scroll(function(e) {
-      var alturaBody = $(window).height();
-      var distanciaElemento = $('.contador').offset().top;
-      var posicaoScroll = $(this).scrollTop();
-      var alturaElemento = $('.contador').outerHeight();
-      
-      if (posicaoScroll > (distanciaElemento+alturaElemento-alturaBody)){
-        ativaContador();
-       } 
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 8000,
+            easing: 'swing',
+            step: function(now) {
+
+                $(this).text(Math.ceil(now));
+            },
+            complete: function() {
+                $(this).stop(true, true)
+            },
+        });
+
     });
+}
+
+$(window).scroll(function(e) {
+
+    var alturaBody = $(window).height();
+    var distanciaElemento = $('.contador').offset().top;
+    var posicaoScroll = $(this).scrollTop();
+    var alturaElemento = $('.contador').outerHeight();
+
+    if (posicaoScroll > (distanciaElemento + alturaElemento - alturaBody)) {
+        ativaContador();
+    }
+
+});
